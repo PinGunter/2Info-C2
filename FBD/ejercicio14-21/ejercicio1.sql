a480ebc3a5bd8f656105eeaf3dbaf65e7320b14f
PinGunter <salvaromero08@gmail.com>
2021-05-21 10:19:28 +0200

    corregido creo

--- FBD/ejercicio14-21/ejercicio1.sql
+++ FBD/ejercicio14-21/ejercicio1.sql
@@ -1,6 +1,6 @@
 -- apartado ñ ---
-select distinct codpie from (select * from ventas natural join 
-(select codpj from proyecto where ciudad='Paris'));
+select codpie from pieza where not exists ((select codpj from proyecto where ciudad='Paris'))
+minus (select codpj from ventas where pieza.codpie=ventas.codpie));
 
 -- apartado o --
 select codpro from (
@@ -18,9 +18,6 @@ not exists(
 );
 
 -- apartado q --
-select codpro from (
-select v1.codpro, v1.codpie from ventas v1 where
-not exists (
-    select codpj from proyecto minus
-    select codpj from ventas v2 where v1.codpro=v2.codpro)
-);
+select codpro from proveedor where not exists (select codpie, codpj from pieza,proyecto where not exists (select codpie,codjp from ventas where ventas.codpro=proveedor.codpro and ventas.codpie = ventas.codpie and ventas.codpj = proyecto.codpj));
+
+select codpro from proveedor where not exists (select codpie, codpj from pieza, proyecto minus (select codpj, codpie from ventas where ventas.codpro = proveedor.codpro ));
